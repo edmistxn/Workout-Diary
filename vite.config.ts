4e07e72d006a957ad20fa111adf8397ec4893cc9
edmistxn <151440857+edmistxn@users.noreply.github.com>
2026-08-29 11:55:59 +0200

    Add files via upload

---
+++ vite.config.ts
@@ -0,0 +1,1 @@
+import{defineConfig}from'vite';import react from'@vitejs/plugin-react';import{VitePWA}from'vite-plugin-pwa';export default defineConfig({plugins:[react(),VitePWA({registerType:'autoUpdate',manifest:{name:'Дневник тренировок',short_name:'Тренировки',display:'standalone',background_color:'#0b0b0c',theme_color:'#0b0b0c',icons:[]}})]),base:'./'});
